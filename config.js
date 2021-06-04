6bf29a79e13b7d66edf7c39919d4d821e8fb00eb
datvc <85302533+datvc@users.noreply.github.com>
2021-06-04 10:56:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Bae à!',
+    introTitle: 'Hưng à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -13,7 +13,7 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Tối nay 7h anh qua đón đi nhậu nhé .',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.google.com/search?q=có+cái+nịt&client=opera&hs=rtj&sxsrf=ALeKk00FYl9N-WMJeO2i6ciw0Def6Km3tg:1622778811854&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiTroSViv3wAhUMEqYKHXykBmUQ_AUoAXoECAEQAw&biw=1326&bih=627#imgrc=JqmE6G0CAULPAM
 }
